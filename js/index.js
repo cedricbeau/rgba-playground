b019f4f982d3d8967b5b83cbf7154c92ad6e430b
cedricbeau <cedric7693@hotmail.fr>
2018-10-20 18:01:40 +0200

    refacto js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
 // -----------------------------------------------------------------------
+// DEFAULT VALUES
+// -----------------------------------------------------------------------
+
+// Default
+let defaultValues = {
+  r: '100',
+  g: '100',
+  b: '250',
+  a: '1'
+}
+
 // RGB
+let defaultRGBValues = "rgb("+ defaultValues.r +", "+ defaultValues.g +", "+ defaultValues.b +")"
+
+// RGBA
+let defaultRGBAValues = "rgb("+ defaultValues.r +", "+ defaultValues.g +", "+ defaultValues.b +", "+ defaultValues.a +")"
+
+// -----------------------------------------------------------------------
+// GET RGB
 // -----------------------------------------------------------------------
 
 function rgbDef(value1, value2, value3, range1, range2, range3, result, txt) {
@@ -8,39 +26,25 @@ function rgbDef(value1, value2, value3, range1, range2, range3, result, txt) {
   value2.innerHTML = range2;
   value3.innerHTML = range3;
 
-  result.style.background = "rgb(" + range1 + ", " + range2 + ", " + range3 + ")";
-  txt.innerHTML = "rgb(" + range1 + ", " + range2 + ", " + range3 + ")";
+  let resultValues = "rgb(" + range1 + ", " + range2 + ", " + range3 + ")";
+  
+  result.style.background = resultValues
+  txt.innerHTML = resultValues
+
 }
 
 function rgbDefFunc() {
+  
   let ranges = document.querySelectorAll(".selector_range")
   let values = document.querySelectorAll(".selector_value")
   let result = document.body
   let txt = document.querySelector('.resultat')
   rgbDef(values[0], values[1], values[2], ranges[0].value, ranges[1].value, ranges[2].value, result, txt)
-}
- 
-function rgbReset(range1, range2, range3, value1, value2, value3, result, txt) {
-  range1.value = '100'
-  range2.value = '100'
-  range3.value = '255'
-  value1.innerHTML = '100'
-  value2.innerHTML = '100'
-  value3.innerHTML = '255'
-  result.style.background = "rgb(100, 100, 255)"
-  txt.innerHTML = "rgb(100, 100, 255)"
-}
 
-function rgbResetFunc() {
-  let ranges = document.querySelectorAll(".selector_range")
-  let values = document.querySelectorAll(".selector_value")
-  let result = document.body
-  let txt = document.querySelector('.resultat')
-  rgbReset(ranges[0],ranges[1],ranges[2],values[0],values[1],values[2],result,txt)
 }
 
 // -----------------------------------------------------------------------
-// RGBA
+// GET RGBA
 // -----------------------------------------------------------------------
 
 function rgbaDef(value1, value2, value3, value4, range1, range2, range3, range4,  result, txt) {
@@ -49,9 +53,11 @@ function rgbaDef(value1, value2, value3, value4, range1, range2, range3, range4,
   value2.innerHTML = range2;
   value3.innerHTML = range3;
   value4.innerHTML = range4;
-  
-  result.style.background = "rgba(" + range1 + ", " + range2 + ", " + range3 + ", " + range4 + ")";
-  txt.innerHTML = "rgba(" + range1 + ", " + range2 + ", " + range3 + ", " + range4 + ")";
+
+  let resultValues = "rgba(" + range1 + ", " + range2 + ", " + range3 + ", " + range4 + ")"
+
+  result.style.background = resultValues
+  txt.innerHTML = resultValues
 
 }
 
@@ -65,33 +71,8 @@ function rgbaDefFunc() {
 
 }
 
-function rgbaReset(range1, range2, range3, range4, value1, value2, value3, value4, result, txt) {
-
-  range1.value = '100'
-  range2.value = '100'
-  range3.value = '255'
-  range4.value = '1'
-  value1.innerHTML = '100'
-  value2.innerHTML = '100'
-  value3.innerHTML = '255'
-  value4.innerHTML = '1'
-  result.style.background = "rgba(100, 100, 255, 1)"
-  txt.innerHTML = "rgba(100, 100, 255, 1)"
-
-}
-
-function rgbaResetFunc() {
-
-  let ranges = document.querySelectorAll(".selector_range")
-  let values = document.querySelectorAll(".selector_value")
-  let result = document.body
-  let txt = document.querySelector('.resultat')
-  rgbaReset(ranges[0],ranges[1],ranges[2],ranges[3],values[0],values[1],values[2],values[3],result,txt)
-
-}
-
 // -----------------------------------------------------------------------
-// DISPLAY
+// DISPLAY DYNAMIC VALUES
 // -----------------------------------------------------------------------
 
 // RGB
@@ -131,12 +112,64 @@ function showValue(value) {
 }
 
 // -----------------------------------------------------------------------
-// RESET
+// RESET VALUES
 // -----------------------------------------------------------------------
+
+// RGB
+function rgbReset(range1, range2, range3, value1, value2, value3, result, txt) {
+
+  range1.value = defaultValues.r
+  range2.value = defaultValues.g
+  range3.value = defaultValues.b
+
+  value1.innerHTML = defaultValues.r
+  value2.innerHTML = defaultValues.g
+  value3.innerHTML = defaultValues.b
+
+  result.style.background = defaultRGBValues
+  txt.innerHTML = defaultRGBValues
+
+}
+
+function rgbResetFunc() {
+  let ranges = document.querySelectorAll(".selector_range")
+  let values = document.querySelectorAll(".selector_value")
+  let result = document.body
+  let txt = document.querySelector('.resultat')
+  rgbReset(ranges[0],ranges[1],ranges[2],values[0],values[1],values[2],result,txt)
+}
+
+// RGBA
+function rgbaReset(range1, range2, range3, range4, value1, value2, value3, value4, result, txt) {
+
+  range1.value = defaultValues.r
+  range2.value = defaultValues.g
+  range3.value = defaultValues.b
+  range4.value = defaultValues.a
+
+  value1.innerHTML = defaultValues.r
+  value2.innerHTML = defaultValues.g
+  value3.innerHTML = defaultValues.b
+  value4.innerHTML = defaultValues.a
+
+  result.style.background = defaultRGBAValues
+  txt.innerHTML = defaultRGBAValues
+
+}
+
+function rgbaResetFunc() {
+
+  let ranges = document.querySelectorAll(".selector_range")
+  let values = document.querySelectorAll(".selector_value")
+  let result = document.body
+  let txt = document.querySelector('.resultat')
+  rgbaReset(ranges[0],ranges[1],ranges[2],ranges[3],values[0],values[1],values[2],values[3],result,txt)
+
+}
 
 function reset() {
   // Get the checkbox
-  let checkBox = document.getElementById("checkOpacity")  
+  let checkBox = document.getElementById("checkOpacity")
   // If the checkbox is checked
   if (checkBox.checked == true) {
     rgbaResetFunc()
